8bf65186fb717d99ed58dd6f74f4d5733ae721b6
Kuuhaku <aim.tantiew@gmail.com>
2024-09-04 01:44:36 +0700

    fix deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+	useCallback,
+	useEffect,
+	useMemo,
+	useState,
+	Suspense,
+} from "react";
 import { gql, useQuery } from "@apollo/client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Box, Typography } from "@mui/material";
@@ -80,59 +86,61 @@ const Home = () => {
 
 	return (
 		<>
-			<Box sx={{ display: "block", py: 5 }}>
-				<Box
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						padding: {
-							xs: "2% 2%",
-							sm: "2% 5%",
-							md: "2% 10%",
-							lg: "2% 20%",
-						},
-						maxWidth: 500,
-						margin: "0 auto",
-					}}
-				>
-					<SearchBar
-						pokemonName={pokemonName}
-						setPokemonName={setPokemonName}
-						onSearch={onSearch}
-					/>
-				</Box>
-			</Box>
-			<Box sx={{ display: "block", pb: 5 }}>
-				<Box
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						padding: {
-							xs: "0 2%",
-							sm: "0 5%",
-							md: "0 10%",
-							lg: "0 20%",
-						},
-						maxWidth: 500,
-						margin: "0 auto",
-					}}
-				>
-					{loading ? (
-						<CardSkeleton />
-					) : pokemon === null ? (
-						<PokemonNotFoundCard />
-					) : pokemon === undefined ? (
-						<TryCard />
-					) : (
-						<PokemonCard
-							pokemon={pokemon}
-							onEvolutionClick={handleEvolutionClick}
+			<Suspense fallback={<div>Loading...</div>}>
+				<Box sx={{ display: "block", py: 5 }}>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							padding: {
+								xs: "2% 2%",
+								sm: "2% 5%",
+								md: "2% 10%",
+								lg: "2% 20%",
+							},
+							maxWidth: 500,
+							margin: "0 auto",
+						}}
+					>
+						<SearchBar
+							pokemonName={pokemonName}
+							setPokemonName={setPokemonName}
+							onSearch={onSearch}
 						/>
-					)}
+					</Box>
 				</Box>
-			</Box>
+				<Box sx={{ display: "block", pb: 5 }}>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							padding: {
+								xs: "0 2%",
+								sm: "0 5%",
+								md: "0 10%",
+								lg: "0 20%",
+							},
+							maxWidth: 500,
+							margin: "0 auto",
+						}}
+					>
+						{loading ? (
+							<CardSkeleton />
+						) : pokemon === null ? (
+							<PokemonNotFoundCard />
+						) : pokemon === undefined ? (
+							<TryCard />
+						) : (
+							<PokemonCard
+								pokemon={pokemon}
+								onEvolutionClick={handleEvolutionClick}
+							/>
+						)}
+					</Box>
+				</Box>
+			</Suspense>
 		</>
 	);
 };
